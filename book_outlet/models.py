1be5a589f61cb07aee729eee02d17f1711b5dc11
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-06-13 01:58:15 +0300

    feat: Book model added.

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    #id = models.AutoField()
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField()
+
+    
