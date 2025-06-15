c245398bcbf435326d2372c8ab73722f6c7fea5e
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-06-15 22:57:53 +0300

    feat: Book list added.

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,2 +1,7 @@
 from django.shortcuts import render
+from .models import Book
 
+def index(request):
+    books = Book.objects.all()
+
+    return render(request, "book_outlet/index.html",{"books": books});
