9eeae4afbd77a85d2dcdf8664dc9b054bb6ad160
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-06-15 23:24:29 +0300

    feat: Dynamic url added

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 
 class Book(models.Model):
     #id = models.AutoField()
@@ -7,6 +8,10 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     isBestSelling = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.id])
+    
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("<int:id>", views.bookDetail)
+    path("<int:id>", views.bookDetail, name="book-detail")
 ]
