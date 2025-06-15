9d3ee7ce753c41198e1cface794732018a5da7a7
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-06-15 23:15:16 +0300

    feat: Detail page added.

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -5,3 +5,8 @@ def index(request):
     books = Book.objects.all()
 
     return render(request, "book_outlet/index.html",{"books": books});
+
+def bookDetail(request, id):
+    book = Book.objects.get(pk=id)
+
+    return render(request, "book_outlet/book_detail.html", {"title": book.title, "author": book.author, "rating": book.rating, "isBestSeller": book.isBestSelling})
