4de48c50383d45539aa08877ecaffbeb25ae4e6c
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-06-15 23:19:08 +0300

    refactor: New 404 object added

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Book
-from django.http import Http404
 
 def index(request):
     books = Book.objects.all()
@@ -8,9 +7,6 @@ def index(request):
     return render(request, "book_outlet/index.html",{"books": books});
 
 def bookDetail(request, id):
-    try:
-        book = Book.objects.get(pk=id)
+    book = get_object_or_404(Book, pk=id)
 
-        return render(request, "book_outlet/book_detail.html", {"title": book.title, "author": book.author, "rating": book.rating, "isBestSeller": book.isBestSelling})
-    except:
-        raise Http404()
+    return render(request, "book_outlet/book_detail.html", {"title": book.title, "author": book.author, "rating": book.rating, "isBestSeller": book.isBestSelling})
