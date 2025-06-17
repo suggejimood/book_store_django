43fce9213415056b4a6d5e7e39432c3a64e4280d
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-06-17 23:35:02 +0300

    feat: Slug added

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 class Book(models.Model):
     #id = models.AutoField()
@@ -8,6 +9,7 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     isBestSelling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.id])
@@ -15,3 +17,7 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
