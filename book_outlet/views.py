6c9d46bfcf06f929f3e70609818827f272fc325a
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-06-18 16:40:55 +0300

    feat: Order and aggregation

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Book
+from django.db.models import Avg
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-rating")
+    numberOfBooks = books.count()
+    avaregeBookRating = books.aggregate(Avg("rating"))
 
-    return render(request, "book_outlet/index.html",{"books": books});
+    return render(request, "book_outlet/index.html",{"books": books, "numberOfBooks": numberOfBooks, "avaregeBookRating": avaregeBookRating});
 
 def bookDetail(request, slug):
     book = get_object_or_404(Book, slug=slug)
