513b69715f33c407c3119c4633976fa12b236f52
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-06-19 23:38:32 +0300

    feat: Book admin page listing updated.

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -3,5 +3,7 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     readonly_fields = ("slug",)
+    list_filter = ("author", "rating",)
+    list_display = ("title", "author", "rating",)
 
 admin.site.register(Book, BookAdmin)
